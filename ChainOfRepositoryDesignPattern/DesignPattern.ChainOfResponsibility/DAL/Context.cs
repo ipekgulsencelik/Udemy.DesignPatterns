a6fba90c9c852a315818dcf2f31b1d6fefb953c2
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-05-04 22:32:46 +0300

    Add project files.

---
+++ ChainOfRepositoryDesignPattern/DesignPattern.ChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPattern.ChainOfResponsibility.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI;database=ChainOfResponsibilityDB;integrated security=true;trusted_connection=true;encrypt=false");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
