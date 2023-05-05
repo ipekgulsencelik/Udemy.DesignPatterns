3dfa0899c53080ac09ec37d41faaa3e9f196de82
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-05-05 18:06:05 +0300

    CQRS Design Pattern (Asp.Net Core 3.1 - SQL)

---
+++ CQRSDesignPattern/DesignPattern.CQRS/CQRSPattern/Queries/GetProductByIDQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPattern.CQRS.CQRSPattern.Queries
+{
+    public class GetProductByIDQuery
+    {
+        public GetProductByIDQuery(int id)
+        {
+            ID = id;
+        }
+
+        public int ID { get; set; }
+    }
+}
---
+++ CQRSDesignPattern/DesignPattern.CQRS/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.CQRS.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI;database=CQRSDB;integrated security=true;trusted_connection=true;encrypt=false");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
